2fdc6c0d129a2c52af2494c236352e997c59856d
Alexander <errsermr@gmail.com>
2023-10-04 00:10:49 +0400

    catalog

---
+++ database/factories/GoodFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Good>
+ */
+class GoodFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'description' => fake()->text(),
+            'path_img' => fake()->image(),
+            'id_catalog' => 1,
+        ];
+    }
+}
