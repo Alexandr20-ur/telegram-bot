57f0d1b44e536e5956f438be76c44ee5c7336ba8
Alexander <errsermr@gmail.com>
2023-10-04 00:10:34 +0400

    catalog

--- routes/console.php
+++ routes/console.php
@@ -1,5 +1,6 @@
 <?php
 
+use DefStudio\Telegraph\Facades\Telegraph;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -18,3 +19,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
+Artisan::command('telegraph:createMenu', function(){
+    Telegraph::registerBotCommands([
+        '/start' => 'Перезапустить бота'
+    ])->send();
+})->purpose('Меню бота');
